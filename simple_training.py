41846c6c02f95c7b9e0ee4d6ff15e2529b748cab
LingYuan <lingyuan.cn@outlook.com>
2019-09-10 10:48:17 +0800

    simple training

--- simple_training.py
+++ simple_training.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # training
 x = tf.placeholder("float", [None, 784])
@@ -28,4 +31,4 @@ correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
