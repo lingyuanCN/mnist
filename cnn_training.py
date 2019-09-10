2df26aea9aad89000d544ab5ab04fed8cda21ee9
LingYuan <lingyuan.cn@outlook.com>
2019-09-10 11:02:30 +0800

    cnn training

--- cnn_training.py
+++ cnn_training.py
@@ -5,6 +5,14 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
 
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
