2ba881903d0c978d6a0924beb894ede73ff55a3c
LingYuan <lingyuan.cn@outlook.com>
2019-09-11 16:59:12 +0800

    upload p62 example

--- p62_example.py
+++ p62_example.py
@@ -7,8 +7,8 @@ batch_size = 8
 w1 = tf.Variable(tf.random_normal([2, 3], stddev = 1, seed = 1))
 w2 = tf.Variable(tf.random_normal([3, 1], stddev = 1, seed = 1))
 
-x = tf.placeholder(tf.float32, shape = (none, 2), name = 'x-input')
-y_ = tf.placeholder(tf.float32, shape = (none, 1), name = 'y-input')
+x = tf.placeholder(tf.float32, shape = (None, 2), name = 'x-input')
+y_ = tf.placeholder(tf.float32, shape = (None, 1), name = 'y-input')
 
 a = tf.matmul(x, w1)
 y = tf.matmul(a, w2)
