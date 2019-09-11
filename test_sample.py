7c212fbf039df7cd9abbf61ec5d9108e6d01a26c
LingYuan <lingyuan.cn@outlook.com>
2019-09-11 09:45:16 +0800

    test sample

--- test_sample.py
+++ test_sample.py
@@ -3,7 +3,12 @@ import tensorflow as tf
 a = tf.constant([1.0, 2.0], name="a")
 b = tf.constant([2.0, 3.0], name="b")
 
-result = a + b
+# result = a + b
+
+g = tf.Graph()
+with g.device('/gpu:0'):
+    result = a + b
+
 
 sess = tf.Session()
 print(sess.run(result))
