708814b02a13ff4c934eac8a4a3e2f1bf5e4a737
LingYuan <lingyuan.cn@outlook.com>
2019-09-12 14:05:51 +0800

    upload mnist optimized

--- mnist_optimized.py
+++ mnist_optimized.py
@@ -98,7 +98,7 @@ def train(mnist):
         "model is %g" % (TRAINING_STEPS, test_acc))
 
 def main(argv=None):
-    mnist = input_data.read_data_sets("/MNIST_data", one_hot = True)
+    mnist = input_data.read_data_sets("/tmp/data", one_hot = True)
     train(mnist)
 
 if __name__ == '__main__':
